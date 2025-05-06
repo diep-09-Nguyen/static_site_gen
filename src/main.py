c0c05b76933935cd5a68be526f8cc39d69e016ee
diep-09-Nguyen <diep.n0994@gmail.com>
2025-05-05 21:34:44 -0500

    Small fix to main

--- src/main.py
+++ src/main.py
@@ -8,8 +8,9 @@ def main():
     basepath = "/"
     if sys.argv[1]:
         basepath = sys.argv[1]
+    print(basepath)
     copytree("static", "docs")
-    generate_page_recursive("content", "template.html", "public", basepath)
+    generate_page_recursive("content", "template.html", "docs", basepath)
 
 
 main()
